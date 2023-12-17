26dce0796bf0f6e48c71e1fb2ec153fa00ee091b
Abelti <abeltibbeshana@gmail.com>
2023-12-17 10:40:24 +0300

    ab-2

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express.Router();
+
+
+app.use(json());
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port ${port}`);
+});
